4a228d3ca6329c22e59efb9b1215f68b0790b0cc
Amrish Pal <amrishp1@gmail.com>
2016-09-08 01:23:52 +0530

    Some small chnages

--- dist/production/assets/js/ip.js
+++ dist/production/assets/js/ip.js
@@ -4,6 +4,7 @@
     });
     $(document).ready(function() {
         $('.page-wrap').scrollbar();
+        $('.menus-wrap').scrollbar();
         $('#nav-icon3').click(function() {
             $(this).toggleClass('open');
             $(".menus-wrap").toggleClass('open');
